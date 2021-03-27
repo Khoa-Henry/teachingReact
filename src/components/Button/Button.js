1ab80a218bc2d426d33947e69545a90117cea353
Khoa Nguyen <knguyen1@amfam.com>
2021-03-26 20:17:56 -0500

    create components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Button.css'
+
+function Button(){
+    return(
+        <div >
+            <button className="button">Click Me</button>
+        </div>
+    )
+}
+export default Button;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Home.css'
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
+import Card from '../../components/Card/Card';
+import {Grid} from '@material-ui/core';
+import Accordion from '../../components/Accordion/Accordion';
 
 function Home(){
     return(
-        <div className="root">
-            <h1>Home Page!!!!!!!!!!!</h1>
-            <p>Intro to React</p>
-        </div>
+        <Grid container spacing={3}>
+            <Button/>
+            <Button/>
+            <Button/>
+            <Button/>
+            <Logo/>
+            <Card/>
+            <Grid item xs={3}>
+                <Accordion/>
+            </Grid>
+        </Grid>
     )
 }
 
