95c0fe1eeab10c3c2e5d795f4a448190714a31d9
Khoa Nguyen <knguyen1@amfam.com>
2021-03-31 20:23:41 -0500

    sidebar

--- src/Nav.js
+++ src/Nav.js
@@ -8,11 +8,13 @@ import AboutYou from './pages/AboutYou/AboutYou';
 import Calendar from './pages/Calendar/Calendar';
 import Home from './pages/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import SideNav from './components/SideNav/SideNav';
 
 export default function nav(){
     return(
         <Router>
-            <NavBar/>
+            {/* <NavBar/> */}
+            <SideNav/>
             <Switch>
                 <Route exact path="/aboutyou">
                     <AboutYou />
